f0a06001189dac69ffa2a4636607fbf1165d3092
Ken2664 <keenkeen41664@gmail.com>
2025-01-13 16:10:12 +0900

    環境変数変更２

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;//PUBLIC
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase URL or Key');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseKey); 
